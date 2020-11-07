a21fa3e09602d7fe6a9a08952c799fe2e73a64fe
sweta sengupta <ssengupta801@gmail.com>
2020-11-06 21:44:36 -0600

    Finished the section and passed the test

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,29 @@
+import math
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
+    pos_list = [i**2 for i in a]
+
+    for num in pos_list:
+        if num in cache:
+            cache[num] += 1
+        else:
+            cache[num] = 1
+    
+    result = []
+
+    for k, v in cache.items():
+        if v == 2:
+            result.append(int(math.sqrt(k)))
+   
     return result
+
+    
 
 
 if __name__ == "__main__":
